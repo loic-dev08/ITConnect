79f614bb775cc1eede1f6b11905ec3be4559ec73
Loic <leotetloic08@gmail.com>
2026-07-06 08:04:07 +0200

    nouvelle mise à jour de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home        from './pages/Home'
-import Search      from './pages/Search'
-import ProProfil   from './pages/ProProfil'
-import Register    from './pages/Inscription'
-import Login       from './pages/Connexion'
-import Dashboard   from './pages/Dashboard'
-import ProDemandes from './pages/ProDemandes'
-import ProtectedRoute from './components/ProtectedRoute'
-import NotFound    from './pages/NotFound'
+import Home              from './pages/Home'
+import Search            from './pages/Search'
+import ProProfil         from './pages/ProProfil'
+import Register          from './pages/Inscription'
+import Login             from './pages/Connexion'
+import Dashboard         from './pages/Dashboard'
+import ProDemandes       from './pages/ProDemandes'
 import ProfilUtilisateur from './pages/ProfilUtilisateur'
+import ProtectedRoute    from './components/ProtectedRoute'
+import NotFound          from './pages/NotFound'
 
 function App() {
   return (
@@ -16,24 +16,21 @@ function App() {
       <Routes>
 
         {/* ── Pages publiques ── */}
-        <Route path="/"           element={<Home />} />
-        <Route path="/recherche"  element={<Search />} />
-        <Route path="/pro/:id"    element={<ProProfil />} />
+        <Route path="/"            element={<Home />} />
+        <Route path="/recherche"   element={<Search />} />
+        <Route path="/pro/:id"     element={<ProProfil />} />
         <Route path="/inscription" element={<Register />} />
-        <Route path="/connexion"  element={<Login />} />
+        <Route path="/connexion"   element={<Login />} />
 
         {/* ── Pages privées (nécessitent d'être connecté) ── */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard"           element={<Dashboard />} />
-          <Route path="/dashboard/demandes"  element={<ProDemandes />} />
-
-          // Dans la zone protégée :
-
-          <Route path="/dashboard/profil"    element={<ProfilUtilisateur />} />
-
-          //A la fin des routes, après toutes les autres :
-          <Route path="*" element={<NotFound />} />
+          <Route path="/dashboard"          element={<Dashboard />} />
+          <Route path="/dashboard/demandes" element={<ProDemandes />} />
+          <Route path="/dashboard/profil"   element={<ProfilUtilisateur />} />
         </Route>
+
+        {/* ── Page 404 ── */}
+        <Route path="*" element={<NotFound />} />
 
       </Routes>
     </BrowserRouter>
