d610162bc150a15fe33294267c34d559995b3484
Loic <leotetloic08@gmail.com>
2026-07-14 14:39:51 +0200

    update 4 de Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,6 +3,7 @@ import { useSearchParams, useNavigate } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import SearchBar from '../components/SearchBar'
+import api from '../services/api'
 import styles from '../css/Recherche.module.css'
 
 // ── Données fictives ──────────────────────────────────────────
