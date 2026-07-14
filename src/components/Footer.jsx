d585975a3d543a1c016ee41b986b5f005d2c418a
Loic <leotetloic08@gmail.com>
2026-07-14 16:11:59 +0200

    mise à jour de la date du Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <span className={styles.logoIt}>IT</span>
         <span className={styles.logoConnect}>Connect</span>
       </div>
-      <p className={styles.copyright}>© 2025 ITConnect — Tous droits réservés</p>
+      <p className={styles.copyright}>© 2026 ITConnect — Tous droits réservés</p>
       <p className={styles.tagline}>
         Mise en relation de professionnels IT · Melun · Sens · Auxerre - Montereau  - Montpellier - Bordeaux
       </p>
